861f60746589c21facca627c47e63b88b8d0b15f
Villins <linshao512@gmail.com>
2017-02-15 14:32:43 +0800

    remove useless line

--- config/config.default.js
+++ config/config.default.js
@@ -1,10 +1,5 @@
 'use strict';
 
-/**
- * mongoose default config
- * @member Config#mongoose
- * @property {String} SOME_KEY - some description
- */
 exports.mongoose = {
   url: '',
   options: {},
