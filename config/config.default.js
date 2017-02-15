ab6f9bc6d4bd0dc9ce200a3897f5dfffe986ed16
Villins <linshao512@gmail.com>
2017-02-15 11:52:52 +0800

    first commit

---
+++ config/config.default.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * mongoose default config
+ * @member Config#mongoose
+ * @property {String} SOME_KEY - some description
+ */
+exports.mongoose = {
+  url: '',
+  options: {},
+};
